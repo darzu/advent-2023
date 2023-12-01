7d21290c38fbff43a5fc205764b2dd8af357a25f
darzu <daryl.zuniga@gmail.com>
2023-12-01 10:36:00 -0800

    day1 part1

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,28 @@
+use std::fs;
+
 pub fn day1() {
     println!("day1");
+
+    let file_path = "./day1/input.txt";
+
+    let input = fs::read_to_string(file_path).unwrap();
+
+    let mut sum = 0;
+    for line in input.lines() {
+        let mut first: u32 = 0;
+        let mut seen_first = false;
+        let mut last: u32 = 0;
+        for c in line.chars() {
+            if let Some(d) = c.to_digit(10) {
+                if !seen_first {
+                    seen_first = true;
+                    first = d;
+                }
+                last = d;
+            }
+        }
+        let val = first * 10 + last;
+        sum += val;
+    }
+    println!("sum: {}", sum);
 }
